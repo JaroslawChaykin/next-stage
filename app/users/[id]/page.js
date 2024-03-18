5717b4f92c01daa02c714a7cf7127e8979766f4c
Jaroslaw <jaroslaw.chaikin@gmail.com>
2024-03-18 11:21:01 +0600

    redirect to 404 from user not found

--- app/users/[id]/page.js
+++ app/users/[id]/page.js
@@ -1,4 +1,5 @@
 import Title from "@/app/components/Title/Title";
+import {notFound} from "next/navigation";
 
 export async function generateMetadata({params, searchParams}) {
     const user = await fetch('https://jsonplaceholder.typicode.com/users/' + params.id)
@@ -13,12 +14,15 @@ export async function generateMetadata({params, searchParams}) {
 const getUser = async (id) => {
     const response = await fetch('https://jsonplaceholder.typicode.com/users/' + id)
         .then(response => response.json())
-
     return response
 }
 
 export default async function User({params}) {
     const user = await getUser(params.id);
+
+    if(Object.keys(user).length === 0) {
+        return notFound()
+    }
 
     return (
         <>
