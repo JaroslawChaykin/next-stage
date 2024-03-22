86943339f6018aa86bcb3ef881900ccf0083c874
Jaroslaw <jaroslaw.chaikin@gmail.com>
2024-03-22 10:30:03 +0600

    create loading for pages

---
+++ app/loading.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Loading = () => {
+    return (
+        <div
+            className={
+                "absolute top-0 left-0 w-svw h-svh bg-black text-white flex justify-center items-center text-6xl"
+            }
+        >
+            Loading...
+        </div>
+    );
+};
+
+export default Loading;
