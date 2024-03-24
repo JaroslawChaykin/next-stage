3b6fbc8372fe7698ba6bec3e283f311f3b77e375
Jaroslaw <jaroslaw.chaikin@gmail.com>
2024-03-24 23:40:26 +0600

    show user posts on page user

--- app/users/[id]/page.js
+++ app/users/[id]/page.js
@@ -1,33 +1,56 @@
+import PostCard from "@/app/components/PostCard/PostCard";
 import Title from "@/app/components/Title/Title";
-import {notFound} from "next/navigation";
+import { notFound } from "next/navigation";
 
-export async function generateMetadata({params, searchParams}) {
-    const user = await fetch('https://jsonplaceholder.typicode.com/users/' + params.id)
-        .then(response => response.json());
+export async function generateMetadata({ params, searchParams }) {
+    const user = await fetch(
+        "https://jsonplaceholder.typicode.com/users/" + params.id
+    ).then((response) => response.json());
 
     return {
         title: user.name,
-        description: 'Страница пользователя ' + user.name,
-    }
+        description: "Страница пользователя " + user.name,
+    };
 }
 
 const getUser = async (id) => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users/' + id)
-        .then(response => response.json())
-    return response
-}
+    const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users/" + id
+    ).then((response) => response.json());
+    return response;
+};
 
-export default async function User({params}) {
+const getUserPosts = async (id) => {
+    const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts?userId=" + id
+    ).then((response) => response.json());
+    return response;
+};
+
+export default async function User({ params }) {
     const user = await getUser(params.id);
+    const posts = await getUserPosts(params.id);
 
-    if(Object.keys(user).length === 0) {
-        return notFound()
+    if (Object.keys(user).length === 0) {
+        return notFound();
     }
 
     return (
         <>
-            <Title Stage='h2' styles={'py-6'}>Пользователь</Title>
-            <p>{user.name}</p>
+            <Title Stage="h2" styles={"py-6"}>
+                {user.name}
+            </Title>
+            <div>
+                <Title Stage="h6" styles={"py-6"}>
+                    Посты
+                </Title>
+                <div>
+                    {posts &&
+                        posts.map((post) => (
+                            <PostCard {...post} key={post.id} />
+                        ))}
+                </div>
+            </div>
         </>
     );
 }
